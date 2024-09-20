353cbfd56b90d87036a2538bd874487d2ad35e2c
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:07 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/onigurumaport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/onigurumaport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/onigurumaport.git'), string(name: 'PORT_DESCRIPTION', value: 'regular expression library' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/onigurumaport.git'), string(name: 'PORT_DESCRIPTION', value: 'regular expression library' )]
   }
 }
